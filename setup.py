0105af981da9a54490f4b3209b0ab975830f26ef
Rikard <rikard.erlandsson@scilifelab.se>
2015-02-18 15:09:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
  
 setup(
   name='access',
-  version='0.0.4',
+  version='0.0.5',
   long_description=__doc__,
   author='Rikard Erlandsson',
   author_email='rikard.erlandson@scilifelab.se',
